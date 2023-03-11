db2eb276b86117ba10e1b36827b59eb7ff03a4f9
HEUKWU <yoo4068@gmail.com>
2023-03-11 15:42:08 +0900

    initial commit

---
+++ src/main/java/hanghae99/rescuepets/RescuePetsApplication.java
@@ -0,0 +1,13 @@
+package hanghae99.rescuepets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RescuePetsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RescuePetsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/hanghae99/rescuepets/RescuePetsApplicationTests.java
@@ -0,0 +1,13 @@
+package hanghae99.rescuepets;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RescuePetsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
