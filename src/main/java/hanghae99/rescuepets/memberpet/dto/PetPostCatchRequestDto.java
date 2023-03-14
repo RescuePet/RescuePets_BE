6749f4f171dc4a192153bee45b23b56415056b56
DESKTOP-JQDL6G8\theCHAIN <khgm234@gmail.com>
2023-03-14 21:05:01 +0900

    Fix : 컴파일 오류 해결 중(4)
    Dto 까지 양호 확인

--- src/main/java/hanghae99/rescuepets/memberpet/dto/PetPostCatchRequestDto.java
+++ src/main/java/hanghae99/rescuepets/memberpet/dto/PetPostCatchRequestDto.java
@@ -1,16 +1,16 @@
-//package hanghae99.rescuepets.memberpet.dto;
-//
-//import java.util.Date;
-//import lombok.Getter;
-//
-//@Getter
-//public class PetPostCatchRequestDto {
-//
-//    private Date postedDate;
-//    private String happenPlace;
-//    private String popfile;
-//    private String kindCd;
-//    private String specialMark;
-//    private String content;
-//    private boolean openNickname;
-//}
+package hanghae99.rescuepets.memberpet.dto;
+
+import java.util.Date;
+import lombok.Getter;
+
+@Getter
+public class PetPostCatchRequestDto {
+
+    private Date postedDate;
+    private String happenPlace;
+    private String popfile;
+    private String kindCd;
+    private String specialMark;
+    private String content;
+    private boolean openNickname;
+}
--- src/main/java/hanghae99/rescuepets/memberpet/dto/PetPostMissingResponseDto.java
+++ src/main/java/hanghae99/rescuepets/memberpet/dto/PetPostMissingResponseDto.java
@@ -1,35 +1,35 @@
-//package hanghae99.rescuepets.memberpet.dto;
-//
-//        import hanghae99.rescuepets.common.entity.PetPostCatch;
-//        import hanghae99.rescuepets.common.entity.PetPostMissing;
-//        import lombok.Builder;
-//        import lombok.Getter;
-//        import java.util.Date;
-//@Getter
-//@Builder
-//public class PetPostMissingResponseDto {
-//
-//    private Date postedDate;
-//    private String happenPlace;
-//    private String popfile;
-//    private String kindCd;
-//    private String specialMark;
-//    private String content;
-//    private String nickname;
-//    private String createdAt;
-//    private String modifiedAt;
-//
-//    public static PetPostCatchResponseDto of(PetPostCatch petPostCatch) {
-//        return PetPostCatchResponseDto.builder()
-//                .postedDate(petPostCatch.getPostedDate())
-//                .happenPlace(petPostCatch.getHappenPlace())
-//                .popfile(petPostCatch.getPopfile())
-//                .kindCd(petPostCatch.getKindCd())
-//                .specialMark(petPostCatch.getSpecialMark())
-//                .content(petPostCatch.getContent())
-//                .nickname(petPostCatch.getMember().getNickname())
-//                .createdAt(petPostCatch.getCreatedAt().toString())
-//                .modifiedAt(petPostCatch.getModifiedAt().toString())
-//                .build();
-//    }
-//}
+package hanghae99.rescuepets.memberpet.dto;
+
+        import hanghae99.rescuepets.common.entity.PetPostCatch;
+        import hanghae99.rescuepets.common.entity.PetPostMissing;
+        import lombok.Builder;
+        import lombok.Getter;
+        import java.util.Date;
+@Getter
+@Builder
+public class PetPostMissingResponseDto {
+
+    private Date postedDate;
+    private String happenPlace;
+    private String popfile;
+    private String kindCd;
+    private String specialMark;
+    private String content;
+    private String nickname;
+    private String createdAt;
+    private String modifiedAt;
+
+    public static PetPostCatchResponseDto of(PetPostCatch petPostCatch) {
+        return PetPostCatchResponseDto.builder()
+                .postedDate(petPostCatch.getPostedDate())
+                .happenPlace(petPostCatch.getHappenPlace())
+                .popfile(petPostCatch.getPopfile())
+                .kindCd(petPostCatch.getKindCd())
+                .specialMark(petPostCatch.getSpecialMark())
+                .content(petPostCatch.getContent())
+                .nickname(petPostCatch.getMember().getNickname())
+                .createdAt(petPostCatch.getCreatedAt().toString())
+                .modifiedAt(petPostCatch.getModifiedAt().toString())
+                .build();
+    }
+}
