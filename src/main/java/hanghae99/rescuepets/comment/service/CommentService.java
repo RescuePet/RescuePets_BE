996ffa839159ea44ea65343719f32364f405b3eb
DESKTOP-JQDL6G8\theCHAIN <khgm234@gmail.com>
2023-03-14 21:21:22 +0900

    Fix : 컴파일 오류 해결 중(12)
    comment Service단 등록, 조회부분만 양호 확인(수정 및 삭제는 컴파일 에러가 아니라 설계오류)

--- src/main/java/hanghae99/rescuepets/comment/service/CommentService.java
+++ src/main/java/hanghae99/rescuepets/comment/service/CommentService.java
@@ -1,56 +1,56 @@
-//package hanghae99.rescuepets.comment.service;
-//
-//import hanghae99.rescuepets.comment.repository.CommentRepository;
-//import hanghae99.rescuepets.comment.dto.CommentRequestDto;
-//import hanghae99.rescuepets.common.dto.ResponseDto;
-//import hanghae99.rescuepets.common.entity.Comment;
-//import hanghae99.rescuepets.common.entity.Member;
-//import hanghae99.rescuepets.common.entity.PetPostMissing;
-//import hanghae99.rescuepets.memberpet.repository.PetPostMissingRepository;
-//import hanghae99.rescuepets.comment.dto.CommentResponseDto;
-//import lombok.RequiredArgsConstructor;
-//import org.springframework.stereotype.Service;
-//
-//import javax.transaction.Transactional;
-//import java.util.ArrayList;
-//import java.util.List;
-//
-//@Service
-//@RequiredArgsConstructor
-//public class CommentService {
-//    private final CommentRepository commentRepository;
-//    private final PetPostMissingRepository petPostMissingRepository;
-//
-//    @Transactional
-//    public ResponseDto<List<CommentResponseDto>> getCommentList(Member member) {
-//        List<Comment> commentList = commentRepository.findAllByUserId(member.getId());
-//        List<CommentResponseDto> comments = new ArrayList<>();
-//        for (Comment comment : commentList) {
-//            comments.add(new CommentResponseDto(comment, member));
-//        }
-//        return ResponseDto.success(comments);
-//    }
-//    @Transactional
-//    public ResponseDto<List<CommentResponseDto>> getCommentCurrentList(Long petPostMissingId) {
-//        List<Comment> commentList = commentRepository.findAllByPostId(petPostMissingId);
-//        List<CommentResponseDto> comments = new ArrayList<>();
-//        for (Comment comment : commentList) {
-//            comments.add(new CommentResponseDto(comment));
-//        }
-//        return ResponseDto.success(comments);
-//    }
-//    @Transactional
-//    public ResponseDto<String> create(Long petPostMissingId, CommentRequestDto requestDto, Member member) {
-//        PetPostMissing petPostMissing = petPostMissingRepository.findById(petPostMissingId).orElseThrow(() -> new NullPointerException("댓글이 없는데용")
-////                CustomException(ErrorCode.NotFoundPost)
-//        );
-//        commentRepository.save(new Comment(requestDto.getContent(), petPostMissing, member));
-//        return ResponseDto.success("댓글 등록 성공");
-//    }
-//
+package hanghae99.rescuepets.comment.service;
+
+import hanghae99.rescuepets.comment.repository.CommentRepository;
+import hanghae99.rescuepets.comment.dto.CommentRequestDto;
+import hanghae99.rescuepets.common.dto.ResponseDto;
+import hanghae99.rescuepets.common.entity.Comment;
+import hanghae99.rescuepets.common.entity.Member;
+import hanghae99.rescuepets.common.entity.PetPostMissing;
+import hanghae99.rescuepets.memberpet.repository.PetPostMissingRepository;
+import hanghae99.rescuepets.comment.dto.CommentResponseDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class CommentService {
+    private final CommentRepository commentRepository;
+    private final PetPostMissingRepository petPostMissingRepository;
+
+    @Transactional
+    public ResponseDto<List<CommentResponseDto>> getCommentList(Member member) {
+        List<Comment> commentList = commentRepository.findAllByMemberId(member.getId());
+        List<CommentResponseDto> comments = new ArrayList<>();
+        for (Comment comment : commentList) {
+            comments.add(new CommentResponseDto(comment, member));
+        }
+        return ResponseDto.success(comments);
+    }
+    @Transactional
+    public ResponseDto<List<CommentResponseDto>> getCommentCurrentList(Long petPostMissingId) {
+        List<Comment> commentList = commentRepository.findAllByPetPostMissingId(petPostMissingId);
+        List<CommentResponseDto> comments = new ArrayList<>();
+        for (Comment comment : commentList) {
+            comments.add(new CommentResponseDto(comment));
+        }
+        return ResponseDto.success(comments);
+    }
+    @Transactional
+    public ResponseDto<String> create(Long petPostMissingId, CommentRequestDto requestDto, Member member) {
+        PetPostMissing petPostMissing = petPostMissingRepository.findById(petPostMissingId).orElseThrow(() -> new NullPointerException("댓글이 없는데용")
+//                CustomException(ErrorCode.NotFoundPost)
+        );
+        commentRepository.save(new Comment(requestDto.getContent(), petPostMissing, member));
+        return ResponseDto.success("댓글 등록 성공");
+    }
+
 //    @Transactional
 //    public ResponseDto<String> update(Long petPostMissingId, CommentRequestDto requestDto, Member member) {
-//        Comment comment = commentRepository.findById(petPostMissingId).orElseThrow(() -> new NullPointerException("댓글이 없는데용")
+//        Comment comment = commentRepository.findById(requestDto.getId()).orElseThrow(() -> new NullPointerException("댓글이 없는데용")
 ////                CustomException(ErrorCode.NotFoundComment)
 //        );
 //        if (comment.getMember().getNickname().equals(member.getNickname())) {
@@ -63,17 +63,17 @@
 //    }
 //
 //    @Transactional
-//    public ResponseDto<String> delete(Long postId, Member member) {
-//        Comment comment = commentRepository.findById(postId).orElseThrow(() -> new NullPointerException("댓글이 없는데용")
+//    public ResponseDto<String> delete(Long petPostMissingId, Member member) {
+//        Comment comment = commentRepository.findById(petPostMissingId).orElseThrow(() -> new NullPointerException("댓글이 없는데용")
 ////                CustomException(ErrorCode.NotFoundComment)
 //        );
 //        if (comment.getMember().getNickname().equals(member.getNickname())) {
-//            commentRepository.deleteById(postId);
+//            commentRepository.deleteById(petPostMissingId);
 //            return ResponseDto.success("댓글 삭제 성공");
 //        } else {
 //            throw new NullPointerException("삭제권한이 없는데용");
 ////                    CustomException(ErrorCode.NoDeletePermission);
 //        }
 //    }
-//
-//}
+
+}
