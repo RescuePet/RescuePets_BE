2d8cecd825fb3220558b343e120c331b50e012a6
HEUKWU <yoo4068@gmail.com>
2023-03-14 23:42:08 +0900

    feat : 사용자 등록 게시물 관심 기능 추가

--- src/main/java/hanghae99/rescuepets/common/entity/Wish.java
+++ src/main/java/hanghae99/rescuepets/common/entity/Wish.java
@@ -1,7 +1,32 @@
 package hanghae99.rescuepets.common.entity;
 
-import javax.persistence.Entity;
-//
-//@Entity
-//public class Wish {
-//}
+import lombok.Builder;
+
+import javax.persistence.*;
+
+@Entity
+public class Wish {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    private Member member;
+
+    @ManyToOne
+    private PetPostCatch catchPost;
+
+    @ManyToOne
+    private PetPostMissing missingPost;
+
+    public Wish(Member member, PetPostCatch postCatch) {
+        this.member = member;
+        this.catchPost = postCatch;
+    }
+
+    public Wish(Member member, PetPostMissing postMissing) {
+        this.member = member;
+        this.missingPost = postMissing;
+    }
+}
--- src/main/java/hanghae99/rescuepets/wish/repository/WishRepository.java
+++ src/main/java/hanghae99/rescuepets/wish/repository/WishRepository.java
@@ -1,4 +1,16 @@
 package hanghae99.rescuepets.wish.repository;
 
-public interface WishRepository {
+import hanghae99.rescuepets.common.entity.Wish;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface WishRepository extends JpaRepository<Wish, Long> {
+    Optional<Wish> findWishByCatchPostIdAndMemberId(Long postId, Long memberId);
+
+    void deleteWishByCatchPostIdAndMemberId(Long postId, Long memberId);
+
+    Optional<Wish> findWishByMissingPostIdAndMemberId(Long postId, Long memberId);
+
+    void deleteWishByMissingPostIdAndMemberId(Long postId, Long memberId);
 }
