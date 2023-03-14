3f3a572661b940c53c5d9b5ea426f911713c80cf
DESKTOP-JQDL6G8\theCHAIN <khgm234@gmail.com>
2023-03-14 21:06:06 +0900

    Fix : 컴파일 오류 해결 중(4)
    Repository 까지 양호 확인

--- src/main/java/hanghae99/rescuepets/memberpet/repository/PetPostMissingRepository.java
+++ src/main/java/hanghae99/rescuepets/memberpet/repository/PetPostMissingRepository.java
@@ -1,9 +1,9 @@
-//package hanghae99.rescuepets.memberpet.repository;
-//import hanghae99.rescuepets.common.entity.PetPostMissing;
-//import org.springframework.data.jpa.repository.JpaRepository;
-//
-//import java.util.List;
-//
-//public interface PetPostMissingRepository extends JpaRepository<PetPostMissing, Long>{
-//    List<PetPostMissing> findAllByMemberId(Long memberId);
-//}
+package hanghae99.rescuepets.memberpet.repository;
+import hanghae99.rescuepets.common.entity.PetPostMissing;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface PetPostMissingRepository extends JpaRepository<PetPostMissing, Long>{
+    List<PetPostMissing> findAllByMemberId(Long memberId);
+}
--- src/main/java/hanghae99/rescuepets/memberpet/repository/WishRepository.java
+++ src/main/java/hanghae99/rescuepets/memberpet/repository/WishRepository.java
@@ -1,14 +1,14 @@
-//package hanghae99.rescuepets.memberpet.repository;
-//
-//import hanghae99.rescuepets.common.entity.Wish;
-//import org.springframework.data.jpa.repository.JpaRepository;
-//
-//import java.util.Optional;
-//
-//public interface WishRepository extends JpaRepository<Wish, Long> {
-//    Optional<Wish> findByPetPostCatchIdAndMemberId(Long petPostCatchId, Long memberId);
-////    void deletePostLikeByPetPostCatchIdAndMemberId(Long memberId, Long petPostCatchId);
-//    void deletePostLikeByPetPostCatchIdAndMemberId(Long petPostCatchId, Long memberId);
-//    Optional<Wish> findByPetPostMissingIdAndMemberId(Long petPostMissingId, Long memberId);
-//    void deletePostLikeByPetPostMissingIdAndMemberId(Long petPostMissingId, Long memberId);
-//}
+package hanghae99.rescuepets.memberpet.repository;
+
+import hanghae99.rescuepets.common.entity.Wish;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface WishRepository extends JpaRepository<Wish, Long> {
+    Optional<Wish> findByPetPostCatchIdAndMemberId(Long petPostCatchId, Long memberId);
+//    void deletePostLikeByPetPostCatchIdAndMemberId(Long memberId, Long petPostCatchId);
+    void deletePostLikeByPetPostCatchIdAndMemberId(Long petPostCatchId, Long memberId);
+    Optional<Wish> findByPetPostMissingIdAndMemberId(Long petPostMissingId, Long memberId);
+    void deletePostLikeByPetPostMissingIdAndMemberId(Long petPostMissingId, Long memberId);
+}
