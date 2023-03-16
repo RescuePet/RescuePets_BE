03ff15988561b11ec4e55ebd0cd3f3fa6604a0cb
SEONGHYUN PARK <sparklep519@gmail.com>
2023-03-16 21:12:49 +0900

    feat:DB 업데이트 및 추가

--- src/main/java/hanghae99/rescuepets/publicpet/service/PublicPetService.java
+++ src/main/java/hanghae99/rescuepets/publicpet/service/PublicPetService.java
@@ -30,6 +30,7 @@ import java.net.URL;
 import java.net.URLEncoder;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Slf4j
 @RequiredArgsConstructor
@@ -43,11 +44,50 @@ public class PublicPetService {
 
 
     @Transactional
+    public String  dataComparison(String pageNo, String state,String size) throws IOException{
+        String apiUrl = createApiUrlTest(pageNo, state, size);
+        JSONArray itemList = fetchDataFromApi(apiUrl);
+        if (itemList == null || itemList.isEmpty()){
+            log.info("API로부터 데이터를 받아오지 못했습니다."); //테스트 로직 변경해야함. 아래 내용 참고하여 필히 변경
+        } //null일경우 앞단에서 수행하기 때문에 saveAndUpdateToDatabase 메서드에서 null체크 없애도 될듯 현재는 saveAndUpdateToDatabase에서 for문 진행시 nullpointerException 발생시킴
+        saveAndUpdateToDatabase(itemList, state);
+        return "비교 성공";
+    }
+
+
+
+    @Transactional
     public String apiSaves(String pageNo, String state) throws IOException {
         String apiUrl = createApiUrl(pageNo, state);
         JSONArray itemList = fetchDataFromApi(apiUrl);
         saveDataToDatabase(itemList, state);
         return "성공";
+    }
+
+    private String createApiUrlTest(String pageNo, String state,String size) throws UnsupportedEncodingException {
+        StringBuilder urlBuilder = new StringBuilder("http://apis.data.go.kr/1543061/abandonmentPublicSrvc/abandonmentPublic");
+        urlBuilder.append("?" + URLEncoder.encode("serviceKey", "UTF-8") + publicApiKey);
+        urlBuilder.append("&" + URLEncoder.encode("bgnde","UTF-8") + "=" + URLEncoder.encode("", "UTF-8")); /*유기날짜(검색 시작일) (YYYYMMDD)*/
+        urlBuilder.append("&" + URLEncoder.encode("endde","UTF-8") + "=" + URLEncoder.encode("", "UTF-8")); /*유기날짜(검색 종료일) (YYYYMMDD)*/
+        urlBuilder.append("&" + URLEncoder.encode("upkind","UTF-8") + "=" + URLEncoder.encode("", "UTF-8")); /*축종코드 (개 : 417000, 고양이 : 422400, 기타 : 429900)*/
+        urlBuilder.append("&" + URLEncoder.encode("kind","UTF-8") + "=" + URLEncoder.encode("", "UTF-8")); /*품종코드 (품종 조회 OPEN API 참조)*/
+        urlBuilder.append("&" + URLEncoder.encode("upr_cd","UTF-8") + "=" + URLEncoder.encode("", "UTF-8")); /*시도코드 (시도 조회 OPEN API 참조)*/
+        urlBuilder.append("&" + URLEncoder.encode("org_cd","UTF-8") + "=" + URLEncoder.encode("", "UTF-8")); /*시군구코드 (시군구 조회 OPEN API 참조)*/
+        urlBuilder.append("&" + URLEncoder.encode("care_reg_no","UTF-8") + "=" + URLEncoder.encode("", "UTF-8")); /*보호소번호 (보호소 조회 OPEN API 참조)*/
+
+        if (state.equals("protect")) {
+            urlBuilder.append("&" + URLEncoder.encode("state", "UTF-8") + "=" + URLEncoder.encode("protect", "UTF-8"));
+        } else if (state.equals("notice")) {
+            urlBuilder.append("&" + URLEncoder.encode("state", "UTF-8") + "=" + URLEncoder.encode("notice", "UTF-8"));
+        } else {
+            urlBuilder.append("&" + URLEncoder.encode("state", "UTF-8") + "=" + URLEncoder.encode("", "UTF-8"));
+        }
+
+        urlBuilder.append("&" + URLEncoder.encode("pageNo", "UTF-8") + "=" + URLEncoder.encode(pageNo, "UTF-8"));
+        urlBuilder.append("&" + URLEncoder.encode("numOfRows","UTF-8") + "=" + URLEncoder.encode(size, "UTF-8")); /*페이지당 보여줄 개수 (1,000 이하), 기본값 : 10*/
+        urlBuilder.append("&" + URLEncoder.encode("_type","UTF-8") + "=" + URLEncoder.encode("json", "UTF-8")); /*xml(기본값) 또는 json*/
+
+        return urlBuilder.toString();
     }
 
     private String createApiUrl(String pageNo, String state) throws UnsupportedEncodingException {
@@ -70,7 +110,7 @@ public class PublicPetService {
         }
 
         urlBuilder.append("&" + URLEncoder.encode("pageNo", "UTF-8") + "=" + URLEncoder.encode(pageNo, "UTF-8"));
-        urlBuilder.append("&" + URLEncoder.encode("numOfRows","UTF-8") + "=" + URLEncoder.encode("1000", "UTF-8")); /*페이지당 보여줄 개수 (1,000 이하), 기본값 : 10*/
+        urlBuilder.append("&" + URLEncoder.encode("numOfRows","UTF-8") + "=" + URLEncoder.encode("1", "UTF-8")); /*페이지당 보여줄 개수 (1,000 이하), 기본값 : 10*/
         urlBuilder.append("&" + URLEncoder.encode("_type","UTF-8") + "=" + URLEncoder.encode("json", "UTF-8")); /*xml(기본값) 또는 json*/
 
         return urlBuilder.toString();
@@ -101,9 +141,11 @@ public class PublicPetService {
         JSONObject jsonObject = new JSONObject(sb.toString());
         JSONObject response = jsonObject.getJSONObject("response");
         JSONObject body = response.getJSONObject("body");
-        JSONObject items = body.getJSONObject("items");
-        JSONArray itemList = items.getJSONArray("item");
-
+        JSONObject items = body.optJSONObject("items");
+        JSONArray itemList = null;
+        if (!items.isEmpty()) {
+            itemList = items.getJSONArray("item");
+        }
         return itemList;
     }
 
@@ -121,6 +163,52 @@ public class PublicPetService {
         }
     }
 
+    //DB 업데이트 및 신규 저장
+    private void saveAndUpdateToDatabase(JSONArray itemList, String state) {
+        for (int i = 0; i < itemList.length(); i++) {
+            JSONObject itemObject = itemList.getJSONObject(i);
+            Optional<PetInfoByAPI> petInfoByAPIOptional = publicPetRepository.findByDesertionNo(itemObject.optString("desertionNo"));
+            PetInfoByAPI petInfoByAPI = petInfoByAPIOptional.orElse(null);
+            if (petInfoByAPIOptional.isEmpty()){
+                log.info("saveAndUpdateToDatabase 메서드에서 if문 save 동작함");
+                PetInfoByAPI petInfo = buildPetInfoTest(itemObject, state);
+                publicPetRepository.save(petInfo);
+            } else if (petInfoByAPI == null){ //이부분 조금 더 생각해볼 필요 있음. if문 진행 관련
+                log.info("saveAndUpdateToDatabase 메서드에서 if문 null 동작함");
+            } else {
+                log.info("saveAndUpdateToDatabase 메서드에서 update 동작함");
+                PetInfoByAPI petInfo = buildPetInfoTest(itemObject, state);
+                petInfoByAPI.update(petInfo); //update 수정 고려
+            }
+        }
+    }
+    private PetInfoByAPI buildPetInfoTest(JSONObject itemObject, String state) {
+        return PetInfoByAPI.builder()
+                .desertionNo(itemObject.optString("desertionNo"))
+                .filename(itemObject.optString("filename"))
+                .happenDt(itemObject.optString("happenDt"))
+                .happenPlace(itemObject.optString("happenPlace"))
+                .kindCd(itemObject.optString("kindCd"))
+                .colorCd(itemObject.optString("colorCd"))
+                .age(itemObject.optString("age"))
+                .weight(itemObject.optString("weight"))
+                .noticeNo(itemObject.optString("noticeNo"))
+                .noticeSdt(itemObject.optString("noticeSdt"))
+                .noticeEdt(itemObject.optString("noticeEdt"))
+                .popfile(itemObject.optString("popfile"))
+                .processState(itemObject.optString("processState"))
+                .sexCd(itemObject.optString("sexCd"))
+                .neuterYn(itemObject.optString("neuterYn"))
+                .specialMark(itemObject.optString("specialMark"))
+                .careNm(itemObject.optString("careNm"))
+                .careTel(itemObject.optString("careTel"))
+                .careAddr(itemObject.optString("careAddr"))
+                .orgNm(itemObject.optString("orgNm"))
+                .chargeNm(itemObject.optString("chargeNm"))
+                .officetel(itemObject.optString("officetel"))
+                .state(state)
+                .build();
+    }
     private PetInfoByAPI buildPetInfoByAPI(JSONObject itemObject) {
         return PetInfoByAPI.builder()
                 .desertionNo(itemObject.optString("desertionNo"))
@@ -145,6 +233,7 @@ public class PublicPetService {
                 .orgNm(itemObject.optString("orgNm"))
                 .chargeNm(itemObject.optString("chargeNm"))
                 .officetel(itemObject.optString("officetel"))
+                .state(itemObject.optString("state"))
                 .build();
     }
 
