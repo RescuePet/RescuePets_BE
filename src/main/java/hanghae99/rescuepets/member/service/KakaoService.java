564ef436e49806d059389708d2a836defd37c871
HEUKWU <yoo4068@gmail.com>
2023-03-17 15:53:52 +0900

    fix : 소셜 로그인 redirect url 수정

--- src/main/java/hanghae99/rescuepets/member/service/KakaoService.java
+++ src/main/java/hanghae99/rescuepets/member/service/KakaoService.java
@@ -73,7 +73,7 @@ public class KakaoService {
         MultiValueMap<String, String> body = new LinkedMultiValueMap<>();
         body.add("grant_type", "authorization_code");
         body.add("client_id", kakaoApiKey); //Rest API 키
-        body.add("redirect_uri", "http://localhost:8080/member/kakao/callback");
+        body.add("redirect_uri", "http://3.38.193.45/member/kakao/callback");
         body.add("code", code);
 
         // HTTP 요청 보내기
