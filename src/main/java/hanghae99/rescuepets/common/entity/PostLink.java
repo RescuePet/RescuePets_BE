98469211c1c0a935e6e5270a9b1fa91db79adec0
DESKTOP-JQDL6G8\theCHAIN <khgm234@gmail.com>
2023-03-22 20:41:00 +0900

    refactor : modify entity
    PostLink 기능 전체 틀 모두 변환 중

--- src/main/java/hanghae99/rescuepets/common/entity/PostLink.java
+++ src/main/java/hanghae99/rescuepets/common/entity/PostLink.java
@@ -17,38 +17,10 @@ public class PostLink {
     @ManyToOne
     @JoinColumn(name = "memberId", nullable = false)
     private Member member;
-    @ManyToOne(fetch = FetchType.EAGER)
-    @JoinColumn(name = "pet_post_catch_id")
-    private PetPostCatch petPostCatchSlotA;
-    @ManyToOne(fetch = FetchType.EAGER)
-    @JoinColumn(name = "petPostMissingIdA", nullable = true)
-    private PetPostMissing petPostMissingSlotA;
-    @ManyToOne(fetch = FetchType.EAGER)
-    @JoinColumn(name = "pet_post_catch_id")
-    private PetPostCatch petPostCatchSlotB;
-    @ManyToOne(fetch = FetchType.EAGER)
-    @JoinColumn(name = "petPostMissingIdB", nullable = true)
-    private PetPostMissing petPostMissingSlotB;
-
-    public PostLink(PetPostCatch petPostCatchSlotA, PetPostCatch petPostCatchSlotB, Member member) {
-        this.member = member;
-        this.petPostCatchSlotA = petPostCatchSlotA;
-        this.petPostCatchSlotB = petPostCatchSlotB;
-    }
-    public PostLink(PetPostCatch petPostCatchSlotA, PetPostMissing petPostMissingSlotB, Member member) {
-        this.member = member;
-        this.petPostCatchSlotA = petPostCatchSlotA;
-        this.petPostMissingSlotB = petPostMissingSlotB;
-    }
-    public PostLink(PetPostMissing petPostMissingSlotA, PetPostCatch petPostCatchSlotB, Member member) {
-        this.member = member;
-        this.petPostMissingSlotA = petPostMissingSlotA;
-        this.petPostCatchSlotB = petPostCatchSlotB;
-    }
-    public PostLink(PetPostMissing petPostMissingSlotA, PetPostMissing petPostMissingSlotB, Member member) {
-        this.member = member;
-        this.petPostMissingSlotA = petPostMissingSlotA;
-        this.petPostMissingSlotB = petPostMissingSlotB;
-    }
-
+    @Column(nullable = false)
+    @Enumerated(value = EnumType.STRING)
+    private PostTypeEnum postType;
+    @Column(nullable = false)
+    @Enumerated(value = EnumType.STRING)
+    private Long linkedPostId;
 }
