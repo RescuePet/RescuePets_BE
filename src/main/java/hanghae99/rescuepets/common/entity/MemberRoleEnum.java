59ae8c241a4dfae8301965f9c3207fc6a3bed674
DESKTOP-JQDL6G8\theCHAIN <khgm234@gmail.com>
2023-04-04 01:14:34 +0900

    Add : add MemberRoleEnum
    멤버 롤 이넘 추가

--- src/main/java/hanghae99/rescuepets/common/entity/MemberRoleEnum.java
+++ src/main/java/hanghae99/rescuepets/common/entity/MemberRoleEnum.java
@@ -1,9 +1,10 @@
 package hanghae99.rescuepets.common.entity;
 
 public enum MemberRoleEnum {
+    ADMIN(Role.ADMIN),  // 관리자
+    MANAGER(Role.MANAGER),  // 매니져(팻벤져스)
     MEMBER(Role.MEMBER),  // 일반회원
-    MANAGER(Role.MANAGER),  // 매니져
-    ADMIN(Role.ADMIN);  // 관리자
+    BADMEMBER(Role.BADMEMBER);  // 불량회원
 
     private final String role;
 
@@ -16,8 +17,9 @@ public enum MemberRoleEnum {
     }
 
     public static class Role {
-        public static final String MEMBER = "ROLE_MEMBER";
-        public static final String MANAGER = "ROLE_MANAGER";
-        public static final String ADMIN = "ROLE_ADMIN";
+        public static final String ADMIN = "관리자";
+        public static final String MEMBER = "일반회원";
+        public static final String MANAGER = "매니져";
+        public static final String BADMEMBER = "불량매니져";
     }
 }
