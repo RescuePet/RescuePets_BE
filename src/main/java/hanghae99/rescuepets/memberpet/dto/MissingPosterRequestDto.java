2d452dcbc0cbcde6694ac2c30f1c3bcb7c2ecb04
DESKTOP-JQDL6G8\theCHAIN <khgm234@gmail.com>
2023-04-05 16:21:18 +0900

    Add : add MissingPosterURL field
    포스트 엔티티에 포스터 URL 주소 필드 추가 및 관련 API get, set 추가

---
+++ src/main/java/hanghae99/rescuepets/memberpet/dto/MissingPosterRequestDto.java
@@ -0,0 +1,11 @@
+package hanghae99.rescuepets.memberpet.dto;
+
+import hanghae99.rescuepets.common.entity.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+public class MissingPosterRequestDto {
+    private String imageURL;
+}
