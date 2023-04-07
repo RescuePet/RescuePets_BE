042cbed0fd99d963801526b41edbafb5eea2b553
SEONGHYUN PARK <sparklep519@gmail.com>
2023-04-07 15:52:14 +0900

    style

--- src/main/java/hanghae99/rescuepets/publicpet/service/ApiScheduler.java
+++ src/main/java/hanghae99/rescuepets/publicpet/service/ApiScheduler.java
@@ -46,7 +46,7 @@ public class ApiScheduler {
     @Value("${kakao.api.keys}")
     private String kakaoMapKey;
 
-    @Scheduled(cron = "20 06 15 * * *")
+    @Scheduled(cron = "0 30 * * * *")
     @Transactional
     protected void apiSchedule() throws IOException {
         log.info("apiSchedule 동작");
