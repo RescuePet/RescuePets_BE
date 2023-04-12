2fd8eb75c62b3974874d1aee1d0606fe7e8893af
DESKTOP-JQDL6G8\theCHAIN <khgm234@gmail.com>
2023-04-12 22:54:16 +0900

    style : modelattribute to requestbody
    게시글 간 링크 생성 할 때 모델 어트리뷰트 말고 리퀘스트 바디로 받게 수정

--- src/main/java/hanghae99/rescuepets/memberpet/dto/PostLinkRequestDto.java
+++ src/main/java/hanghae99/rescuepets/memberpet/dto/PostLinkRequestDto.java
@@ -1,12 +1,15 @@
 package hanghae99.rescuepets.memberpet.dto;
 
 import hanghae99.rescuepets.common.entity.*;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.validation.constraints.Pattern;
 
 @Getter
+@NoArgsConstructor
 public class PostLinkRequestDto {
     @Pattern(regexp = "^[1-9]\\d*$", message = "양수만 입력 가능합니다.")
     private Long linkedPostId;
